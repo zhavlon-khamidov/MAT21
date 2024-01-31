85ce8a49d481234529cb9c3d66c1726c87500d9f
Mr.Zhavlon <zhavlon.khamidov@alatoo.edu.kg>
2024-01-31 08:40:07 +0600

    Hello World app and first entitiy

---
+++ task-manager/src/main/java/kg/alatoo/taskmanager/TaskManagerApplication.java
@@ -0,0 +1,14 @@
+package kg.alatoo.taskmanager;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagerApplication.class, args);
+	}
+
+}
---
+++ task-manager/src/main/java/kg/alatoo/taskmanager/entities/Task.java
@@ -0,0 +1,39 @@
+package kg.alatoo.taskmanager.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Task {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String title;
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
