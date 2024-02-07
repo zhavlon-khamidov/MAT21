10dbb7a3e17a3493e5e7aceec05d81e523fa7f52
Mr.Zhavlon <zhavlon.khamidov@alatoo.edu.kg>
2024-02-07 09:03:19 +0600

    CRUD done, HW: PATCH request

--- task-manager/src/main/java/kg/alatoo/taskmanager/controllers/ApiController.java
+++ task-manager/src/main/java/kg/alatoo/taskmanager/controllers/ApiController.java
@@ -2,10 +2,14 @@ package kg.alatoo.taskmanager.controllers;
 
 import kg.alatoo.taskmanager.entities.Task;
 import kg.alatoo.taskmanager.repositories.TaskRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/api/v1/")
 public class ApiController {
 
     private final TaskRepository taskRepository;
@@ -14,14 +18,48 @@ public class ApiController {
         this.taskRepository = taskRepository;
     }
 
-
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello, World!";
-    }
-
-    @GetMapping("/api/v1/task")
+    @GetMapping("task")
     public Iterable<Task> getAllTasks() {
         return taskRepository.findAll();
     }
+
+
+    @GetMapping("task/{id}")
+    public ResponseEntity<Task> getTaskById(@PathVariable Long id) {
+        Optional<Task> optionalTask = taskRepository.findById(id);
+        return optionalTask
+                .map(task -> ResponseEntity.ok().body(task))
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+//    @RequestMapping(method = RequestMethod.POST, path = "task")
+    @PostMapping("task")
+    public Task createTask(@RequestBody Task task) {
+        task.setId(null);
+        return taskRepository.save(task);
+    }
+
+    @PutMapping("task/{id}")
+    public Task updateTask(@RequestBody Task task, @PathVariable Long id) {
+        if (!taskRepository.existsById(id)) {
+            throw new NoSuchElementException();
+        }
+        task.setId(id);
+        return taskRepository.save(task);
+    }
+
+    @DeleteMapping("task/{id}")
+    public ResponseEntity<Task> deleteTaskById(@PathVariable Long id) {
+        Optional<Task> optionalTask = taskRepository.findById(id);
+        if (optionalTask.isPresent()) {
+            taskRepository.deleteById(id);
+            return ResponseEntity.ok().body(optionalTask.get());
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    //TODO: Create PATCH request for update
+
+
 }
