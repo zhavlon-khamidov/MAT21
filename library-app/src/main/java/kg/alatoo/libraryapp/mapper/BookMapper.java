9f2a650704e6e49d290ea2ecca9076e45d53a68e
Mr.Zhavlon <zhavlon.khamidov@alatoo.edu.kg>
2024-02-27 14:39:22 +0600

    Explained DTOs and MapStruct

--- library-app/src/main/java/kg/alatoo/libraryapp/mapper/BookMapper.java
+++ library-app/src/main/java/kg/alatoo/libraryapp/mapper/BookMapper.java
@@ -3,13 +3,16 @@ package kg.alatoo.libraryapp.mapper;
 import kg.alatoo.libraryapp.dto.BookDTO;
 import kg.alatoo.libraryapp.entities.Book;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
+import org.mapstruct.Mapping;
 
 @Mapper
 public interface BookMapper {
 
     BookDTO bookToBookDto(Book book);
 
+
+    @Mapping(target = "publisher", ignore = true)
+    @Mapping(target = "authors", ignore = true)
     Book bookDtoToBook(BookDTO dto);
 
 }
